b7d56880e0df1ba676afcf87683a3d1ca8fed070
Serheii <mail.sergey.ka@gmail.com>
2023-02-10 00:00:41 +0300

    final

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ start.value="START";
 start.setAttribute('onclick','startGame()');
 start.id='start';
 
+let scoreNum = {
+    L:0,
+    R:0
+}
 let score=document.createElement('div');
 document.body.appendChild(score);
-score.value="SCORE";
+score.innerHTML="SCORE: " + scoreNum.L + ' : ' + scoreNum.R;
 score.id='score';
 
 let gameArea=document.createElement('div');
@@ -59,10 +63,11 @@ let rightB={
 }
 
 let ballMove={
-    speedX:10,
+    speedX:0,
     speedY:0,
     
     update:function () {
+
                     //шарик дошёл до левого края
             if (((parseFloat(ball.style.marginLeft) - gameBoardLeft.offsetWidth + this.speedX) <= 0)
         ||          //шарик дошёл до правого края
@@ -78,18 +83,21 @@ let ballMove={
                     ball.style.marginLeft = gameArea.offsetWidth - ball.offsetWidth - gameBoardRight.offsetWidth + 'px';    //дожимаем шарик к ракетке
                     this.speedX = -this.speedX; //отскок от ракетки
                     
-                    setTimeout(startGame,40);   //продолжаем
+                    setTimeout(game,40);   //продолжаем
 
                     if (rightB.speed>0)  //подкрутка правой ракеткой
-                        this.speedY++;
+                        this.speedY+=3;
                     else if (rightB.speed<0)
-                        this.speedY--;
+                        this.speedY-=3;
                     
                 } else {
                     // попадание в правую стенку
                     ball.style.marginLeft = gameArea.offsetWidth - ball.offsetWidth + 'px';    //дожимаем шарик к стене
                     this.speedX = 0;
                     this.speedY = 0;
+                    scoreNum.L++;
+                    score.innerHTML="SCORE: " + scoreNum.L + ' : ' + scoreNum.R;
+                    start.disabled="";
                 }
 
             } else {                //шарик летит влево
@@ -101,18 +109,21 @@ let ballMove={
                     ball.style.marginLeft = gameBoardLeft.offsetWidth + 'px';    //дожимаем шарик к ракетке
                     this.speedX = -this.speedX; //отскок от ракетки
                     
-                    setTimeout(startGame,40);   //продолжаем
+                    setTimeout(game,40);   //продолжаем
 
                     if (leftB.speed>0)  //подкрутка левой ракеткой
-                    this.speedY++;
+                    this.speedY+=3;
                     else if (leftB.speed<0)
-                    this.speedY--;
+                    this.speedY-=3;
                     
                 } else {
                     // попадание в левую стенку
                     ball.style.marginLeft = 0 + 'px';    //дожимаем шарик к стене
                     this.speedX = 0;
                     this.speedY = 0;
+                    scoreNum.R++;
+                    score.innerHTML="SCORE: " + scoreNum.L + ' : ' + scoreNum.R;
+                    start.disabled="";
                 }
             }
 
@@ -126,7 +137,7 @@ let ballMove={
                 ball.style.marginLeft=parseFloat(ball.style.marginLeft) + this.speedX + 'px';
                 ball.style.marginTop=parseFloat(ball.style.marginTop) + this.speedY + 'px';
 
-                setTimeout(startGame,40);   //продолжаем
+                setTimeout(game,40);   //продолжаем
         }
     }
 }
@@ -161,9 +172,29 @@ document.onkeyup = function (eo) {
     }
 }
 
-let timerId;
+function randomDiap(n,m) {
+    return Math.floor(Math.random()*(m-n+1))+n;
+}
 
 function startGame() {
+    //прячем кнопку
+    start.disabled="disabled";
+    //ракетки на позиции
+    gameBoardRight.style.marginTop=gameArea.offsetHeight/2 - gameBoardRight.offsetHeight/2 + 'px';        
+    gameBoardLeft.style.marginTop=gameArea.offsetHeight/2 - gameBoardLeft.offsetHeight/2 + 'px';
+    //шарик на центр
+    ball.style.marginLeft=gameArea.offsetWidth/2 - ball.offsetWidth/2 + 'px';
+    ball.style.marginTop=gameArea.offsetHeight/2 - ball.offsetHeight/2 + 'px';
+    //определяем случайную скорость и направление шарика
+    ballMove.speedX=randomDiap(-10,10);
+    ballMove.speedY=randomDiap(-3,3);
+
+    setInterval(()=>ballMove.speedX++,20000)//увеличиваем скорость через интервал
+
+    setTimeout(game,3000);
+}
+
+function game() {
     leftB.update();
     rightB.update();
     ballMove.update();
